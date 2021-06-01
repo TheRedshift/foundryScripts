b66e67c1283d31412718ff66225880ed0e3f0903
TheRedshift <rsoni28124@gmail.com>
2021-06-01 22:22:45 +0100

    update divine smite to count weak targets and improved divine smite

--- purposeSmite.js
+++ purposeSmite.js
@@ -10,12 +10,10 @@ let s_actor = canvas.tokens.controlled[0]?.actor || game.user.character;
 if (hasAvailableSlot(s_actor)) {
     let optionsText = "";
     for (let i = 1; i < maxSpellSlot; i++) {
-        const slots = getSpellSlots(s_actor, i);
-        if (slots.value > 0) {
-            const level = CONFIG.DND5E.spellLevels[i];
-            const label = game.i18n.format('DND5E.SpellLevelSlot', {level: level, n: slots.value});
-            optionsText += `<option value="${i}">${label}</option>`;
-        }
+        const level = CONFIG.DND5E.spellLevels[i];
+        const label = "Level " + i;
+        optionsText += `<option value="${i}">${label}</option>`;
+        
     }
 
     let dialogEditor = new Dialog({
@@ -31,19 +29,11 @@ if (hasAvailableSlot(s_actor)) {
             </div>
             <div class="form-group">
                 <label class="checkbox">
-                <input type="checkbox" name="consumeCheckbox" checked/>` + game.i18n.localize("DND5E.SpellCastConsume") + `</label>
-            </div>
-            <div class="form-group">
-                <label class="checkbox">
-                <input type="checkbox" name="improvedSmiteCheckbox checked"/>` + "Improved Divine Smite?" + `</label>
-            </div>
-            <div class="form-group">
-                <label class="checkbox">
                 <input type="checkbox" name="criticalCheckbox"/>` + game.i18n.localize("DND5E.CriticalHit") + "?" + `</label>
             </div>
             <div class="form-group">
                 <label class="checkbox">
-                <input type="checkbox" name="weakCheckbox"/>` + "Fiend or Undead?" + `</label>
+                <input type="checkbox" name="outerTargetCheckbox"/>` + game.i18n.localize("Outer Target") + "?" + `</label>
             </div>
         </form>
         `,
@@ -64,11 +54,8 @@ if (hasAvailableSlot(s_actor)) {
             if (confirmed) {
                 const slotLevel = parseInt(html.find('[name=slot-level]')[0].value);
                 const criticalHit = html.find('[name=criticalCheckbox]')[0].checked;				
-                const consumeSlot = html.find('[name=consumeCheckbox]')[0].checked;
-                const improvedSmite = html.find('[name=improvedSmiteCheckbox]')[0].checked;
-                const weak = html.find('[name=weakCheckbox]')[0].checked;
-
-                smite(s_actor, slotLevel, criticalHit, consumeSlot. improvedSmite, weak);
+                const outerTarget = html.find('[name=outerTargetCheckbox]')[0].checked;
+                smite(s_actor, slotLevel, criticalHit, outerTarget);
             }
         }
     }).render(true);
@@ -90,17 +77,10 @@ function getSpellSlots(actor, level) {
 }
 
 /**
- * Returns whether the actor has any spell slot left.
- * @param {Actor5e} actor - the actor to get slot information from.
- * @returns {boolean} True if any spell slots of any spell level are available to be used.
+ * Returns true as purpose doesn't consume spell slots to smite
  */
  function hasAvailableSlot(actor) {
-    for (let slot in actor.data.data.spells) {
-        if (actor.data.data.spells[slot].value > 0) {
-            return true;
-        }
-    }
-    return false;
+    return true;
  }
 
   
@@ -109,25 +89,15 @@ function getSpellSlots(actor, level) {
  * @param {Actor5e} actor - the actor that is performing the action.
  * @param {integer} slotLevel - the spell slot level to use when smiting.
  * @param {boolean} criticalHit - whether the hit is a critical hit.
- * @param {boolean} consume - whether to consume the spell slot.
- * @param {boolean} improvedSmite - whether the hit does an extra 1d8 from the Paladin class feature
- * @param {boolean} weak - whether the target takes an extra 1d8 from being a Fiend or Undead
  */
 
-  function smite(actor, slotLevel, criticalHit, consume, improvedSmite, weak) {
-    let chosenSpellSlots = getSpellSlots(actor, slotLevel);
-
-    if (chosenSpellSlots.value < 1) {
-        ui.notifications.error("No spell slots of the required level available.");
-        return;
-    }
+  function smite(actor, slotLevel, criticalHit) {
 
     let numDice = slotLevel + 1;
     if (criticalHit) numDice *= 2;
-    if (improvedSmite) numDice += 1;
-    if (weak) numDice += 1;
-    const flavor = `Macro Divine Smite - ${game.i18n.localize("DND5E.DamageRoll")} (${game.i18n.localize("DND5E.DamageRadiant")})`;
+    const flavor = `Purpose Divine Smite - ${game.i18n.localize("DND5E.DamageRoll")} (${game.i18n.localize("DND5E.DamageRadiant")})`;
     new Roll(`${numDice}d8`).roll().toMessage({ flavor: flavor, speaker });
+    
     
 
     if (consume){
