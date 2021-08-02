b24f0be48f588f212f3abce42a78569f36ae7f51
David Knapman <dai.knapz@gmail.com>
2021-08-03 00:13:25 +0100

    Updated octavius saber to now take in rogue level of current character

--- octaviusSaber.js
+++ octaviusSaber.js
@@ -1,4 +1,6 @@
-let dice = new Roll('(ceil(13 /2))d6');
+let s_actor = canvas.tokens.controlled[0]?.actor || game.user.character; 
+
+const dice = new Roll('(ceil(@rogueLevel /2))d6', {rogueLevel: getRogueLevel(s_actor) });
 dice.roll();
 
 dice.terms[0].results.forEach(result => {
@@ -7,4 +9,14 @@ dice.terms[0].results.forEach(result => {
     }
 });
 
-await dice.toMessage({flavor: "Sneak Attack"});
+await dice.toMessage({flavor: "Sneak Attack", speaker});
+
+
+/**
+ * Gets the rogue levels for a particular actor.
+ * @param {Actor5e} actor - the actor to get slot information from.
+ * @returns {number} The number of levels.
+ */
+function getRogueLevel(actor) {
+    return actor.data.data.classes.rogue.levels;
+}
