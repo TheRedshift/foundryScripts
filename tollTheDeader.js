ac4b431f0a619cfc7c4bb9dbcb1d437b459a4b94
David Knapman <dai.knapz@gmail.com>
2022-01-09 20:11:43 +0000

    Added and fixed toll the deader

---
+++ tollTheDeader.js
@@ -0,0 +1,36 @@
+let dialogEditor = new Dialog({
+    title: `Toll the Deader`,
+    content: `Is the target damaged?`,
+    buttons: {
+      torch: {
+        label: `No`,
+        callback: async () => {
+          let target = canvas.tokens.controlled[0]?.actor;
+          const save = await target.rollAbilitySave("wis");
+          let roll = new Roll(`3d8`);
+          roll = await roll.roll({async: true});
+          await roll.toMessage({
+              flavor: "d8 damage",
+          });
+        }
+      },
+      light: {
+        label: `Yes`,
+        callback: async () => {
+          let roll = new Roll(`3d12`);
+          roll = await roll.roll({async: true});
+          await roll.toMessage({
+              flavor: "d12 damage",
+          });
+        }
+      },
+      close: {
+        icon: "<i class='fas fa-tick'></i>",
+        label: `Close`
+      },
+    },
+    default: "close",
+    close: () => {}
+  });
+  
+  dialogEditor.render(true);
