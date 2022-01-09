80ea6eb6c2c42ec1ed7fdb66ebb17b4197697baf
David Knapman <dai.knapz@gmail.com>
2022-01-09 19:24:42 +0000

    Imrpoved async handling of dogs attacking

--- goodBoisAttack.js
+++ goodBoisAttack.js
@@ -1,30 +1,36 @@
 new Dialog({
-    title:'Example Dialog',
-    content:`
-      <form>
-        <div class="form-group">
-          <label>Input text</label>
-          <input type='text' name='inputField'></input>
-        </div>
-      </form>`,
-    buttons:{
-      yes: {
-        icon: "<i class='fas fa-check'></i>",
-        label: `Apply Changes`
-      }},
-    default:'yes',
-    close: async html => {
-      let result = html.find('input[name=\'inputField\']');
-      var i;
-      var roll;
-      for (i = 0; i < result.val(); i++) {
-        roll = new Roll(`2d20`);
-        roll = await roll.evaluate({async: true});
-        await roll.toMessage({
-          flavor: "Doggo Attacks!"},
-          {
-              create: true
-          });
-  };
-  }
+	title:'How many good bois are attacking?',
+	content:`
+	  <form>
+		<div class="form-group">
+		  <label>Doggos</label>
+		  <input type='number' name='inputField'></input>
+		</div>
+	  </form>`,
+	buttons:{
+	  yes: {
+		icon: "<i class='fas fa-check'></i>",
+		label: `Confirm`
+	  }},
+	default:'yes',
+	close: async html => {
+		const result = html.find('input[name=\'inputField\']');
+		let chatMessages = [];
+		const doggos = result.val()
+
+		for (let i = 0; i < doggos; i++) {
+			let roll = new Roll(`2d20`);
+			roll = await roll.evaluate({async: true});
+			chatMessages.push(
+				roll.toMessage({
+					flavor: `Doggo ${i}/${doggos} Attacks!`
+				},
+				{
+					create: true
+				})
+			);
+		}
+		
+		await Promise.allSettled(chatMessages);
+}
 }).render(true);
